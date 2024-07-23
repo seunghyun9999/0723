f6bb0584f122d101fd7e7f50b2554620dc875998
seunghyun9999 <sean1177@naver.com>
2024-07-23 14:05:54 +0900

    아임 마스터

---
+++ 0723.py
@@ -0,0 +1,10 @@
+class aa :
+    def __init__(self,x,y,z):
+        self.x1=x
+        self.x2=y
+        self.x3=z
+    def bb (self) :
+        print(self.x1,self.x2,self.x3)
+
+dd=aa('가가','나나','다다')
+dd.bb()
---
+++ test.py
@@ -0,0 +1,6 @@
+def errorp() :
+    try :
+        10/0
+
+    except :
+        print('에러있음')
